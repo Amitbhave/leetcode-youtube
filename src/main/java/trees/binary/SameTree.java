2ea2c4fabd545a770bb4e5eb298a481672509556
Amit Bhave <amitbhave10@gmail.com>
2022-05-23 22:40:12 +0530

    5. Same Tree

---
+++ src/main/java/trees/binary/SameTree.java
@@ -0,0 +1,20 @@
+package trees.binary;
+
+import trees.TreeNode;
+
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.right, q.right) &&
+                isSameTree(p.left, q.left);
+    }
+
+}
