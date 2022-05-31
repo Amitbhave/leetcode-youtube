e5a65898a0af0e8002d329bca112ce9a123bcc25
Amit Bhave <amitbhave10@gmail.com>
2022-05-31 23:18:40 +0530

    9. Binary Search

---
+++ src/main/java/search/BinarySearch.java
@@ -0,0 +1,24 @@
+package search;
+
+public class BinarySearch {
+
+    public static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (target > nums[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+}
